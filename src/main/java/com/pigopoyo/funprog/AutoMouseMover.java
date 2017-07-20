2ead3fa85da2dc2ca07ac3fc49fcbe52a8a30e42
gopiraj24 <gopiraj.dorairaj@gmail.com>
2017-07-20 08:23:21 -0400

    fun programing

---
+++ src/main/java/com/pigopoyo/funprog/AutoMouseMover.java
@@ -0,0 +1,21 @@
+package com.pigopoyo.funprog;
+
+import java.awt.*;
+import java.util.Random;
+
+public class AutoMouseMover {
+
+    /**
+     * Starting point for the JVM.
+     */
+    public static void main(String argument[]) throws AWTException, InterruptedException {
+
+        Random random = new Random();
+        Robot robot = new Robot();
+        while (true) {
+            robot.mouseMove(random.nextInt(900),random.nextInt(800));
+            Thread.sleep(5000);
+        }
+
+    }
+}
